e1fb89b438ce2b8a191436bc89786ed4a734e38f
Administrator <admin@example.com>
2019-04-25 14:35:23 +0530

    intial  commit

---
+++ sample.java
@@ -0,0 +1,7 @@
+public class HelloWorldException {
+    public static void main (String[] args) throws Exception {
+        System.out.println("Bienvenitos!");
+        throw new Exception("Generic Exception");
+    }
+}
+
